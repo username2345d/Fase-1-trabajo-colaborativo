d5e192740cec994cda35495f35fa17977186cfcd
username2345d <148638922+username2345d@users.noreply.github.com>
2026-04-13 16:29:42 -0500

    start text system

---
+++ visual novel/Assets/Scripts/Scene01/Scene01Events.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene01Events : MonoBehaviour
+{
+
+    public GameObject FadeScreenIn;
+    public GameObject charFhós;
+    public GameObject charFhósSpeak1;
+    public GameObject charFhósSpeak2;
+    public GameObject charNpc;
+
+    void Start()
+    {
+        StartCoroutine(EventStarter());
+    }
+
+    // This coroutine controls the entire initial sequence of Scene 01.
+    // It is the main part of the events in this scene (intro cinematic) and character timings.
+    IEnumerator EventStarter() 
+    {
+        yield return new WaitForSeconds(2);
+        FadeScreenIn.SetActive(false);
+
+        yield return new WaitForSeconds(1);
+        charFhós.SetActive(true);
+
+        yield return new WaitForSeconds(2);
+        //this is where our text function go will go in future
+
+        yield return new WaitForSeconds(6);
+        charFhós.SetActive(false);
+        charFhósSpeak1.SetActive(true);
+
+        yield return new WaitForSeconds(6);
+        charFhósSpeak1.SetActive(false);
+        charFhósSpeak2.SetActive(true);
+
+        yield return new WaitForSeconds(5);
+        charFhósSpeak2.SetActive(false);
+        charFhós.SetActive(true);
+
+        yield return new WaitForSeconds(1);
+        charNpc.SetActive(true);
+
+    }
+
+
+}
