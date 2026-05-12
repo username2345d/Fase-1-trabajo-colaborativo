e5882d9466502dd3fcdd133b8f3d4610d5d4a570
juan guillermo piñerez <148638922+username2345d@users.noreply.github.com>
2026-05-11 22:59:52 -0500

    scene 2 script fix

--- visual novel/Assets/Scripts/Scene02/Scene02Events.cs
+++ visual novel/Assets/Scripts/Scene02/Scene02Events.cs
@@ -1,49 +1,156 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Scene02Events : MonoBehaviour
 {
+    // === Fades ===
+    public GameObject FadeScreenIn;
+    public GameObject FadeOut;
 
-    public GameObject textBox;
-    [SerializeField] GameObject FadeScreenIn;
-    [SerializeField] GameObject charThalassaSpeakAnim;
+    // === Characters ===
+    public GameObject charFhósIdle;
+    public GameObject charFhósSpeak1;
+    public GameObject charFhósSpeak2;
+    public GameObject charFhósAnim;
+
+    public GameObject charThalassaIdle;
+    public GameObject charThalassaSpeak1;
+    public GameObject charThalassaSpeakAnim;
+
+    public GameObject charVarkasIdle;
+    public GameObject charVarkasSpeak;
+    public GameObject charVarkasAnim;
+
+
+    // === UI ===
+    [SerializeField] GameObject textBox;
+    [SerializeField] GameObject mainTextObject;
+    [SerializeField] GameObject nextButton;
+    [SerializeField] GameObject charName;
+
     [SerializeField] string textToSpeak;
     [SerializeField] int currentTextLenght;
     [SerializeField] int textLenght;
-    [SerializeField] GameObject mainTextObject;
-    [SerializeField] GameObject nextButton;
+
     [SerializeField] int eventPos = 0;
+
+    void Update()
+    {
+        textLenght = TextCreator.charCount;
+    }
 
     void Start()
     {
         StartCoroutine(EventStarter());
     }
 
-    void Update()
-    {
-        
-    }
-
     IEnumerator EventStarter()
     {
-        //event 0
-        yield return new WaitForSeconds(2);
+        //event 0 - Inicio de escena
+        yield return new WaitForSeconds(1.5f);
         FadeScreenIn.SetActive(false);
 
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(1f);
+
         charThalassaSpeakAnim.SetActive(true);
+        charFhósAnim.SetActive(true);
+        
         mainTextObject.SetActive(true);
-        textToSpeak = "Fhós… You can still stop this. You don’t have to keep carrying everything alone.";
+        textBox.SetActive(true);
+
+        charName.GetComponent<TMPro.TMP_Text>().text = "Thalassa";
+        textToSpeak = "Fhós… You can still stop this.\r\nYou don’t have to keep carrying everything alone.";
         textBox.GetComponent<TMPro.TMP_Text>().text = textToSpeak;
         currentTextLenght = textToSpeak.Length;
         TextCreator.runTextPrint = true;
+
         yield return new WaitForSeconds(0.05f);
         yield return new WaitForSeconds(1);
         yield return new WaitUntil(() => textLenght == currentTextLenght);
-        yield return new WaitForSeconds(0.05f);
+        yield return new WaitForSeconds(0.5f);
+
         nextButton.SetActive(true);
         eventPos = 1;
+    }
 
+    IEnumerator EventOne()
+    {
+        nextButton.SetActive(false);
+        charThalassaSpeakAnim.SetActive(false);
+        charThalassaIdle.SetActive(true);
+
+        charFhósAnim.SetActive(false);
+        charFhósSpeak1.SetActive(true);
+
+        charName.GetComponent<TMPro.TMP_Text>().text = "Fhós";
+        textToSpeak = "It’s not that simple.\r\nIf I let go now, everything falls apart.";
+        textBox.GetComponent<TMPro.TMP_Text>().text = textToSpeak;
+        currentTextLenght = textToSpeak.Length;
+        TextCreator.runTextPrint = true;
+
+        yield return new WaitForSeconds(0.05f);
+        yield return new WaitForSeconds(1);
+        yield return new WaitUntil(() => textLenght == currentTextLenght);
+        yield return new WaitForSeconds(0.5f);
+
+        nextButton.SetActive(true);
+        eventPos = 2;
+    }
+
+    IEnumerator EventTwo()
+    {
+        nextButton.SetActive(false);
+        charFhósSpeak1.SetActive(false);
+        charFhósIdle.SetActive(true);
+        charThalassaIdle.SetActive(false);
+        charThalassaSpeak1.SetActive(true);
+
+        charName.GetComponent<TMPro.TMP_Text>().text = "Thalassa";
+        textToSpeak = "Look at yourself. That light is consuming you alive.\r\nHow much longer do you think you can endure this?";
+        textBox.GetComponent<TMPro.TMP_Text>().text = textToSpeak;
+        currentTextLenght = textToSpeak.Length;
+        TextCreator.runTextPrint = true;
+
+        yield return new WaitForSeconds(0.05f);
+        yield return new WaitForSeconds(1);
+        yield return new WaitUntil(() => textLenght == currentTextLenght);
+        yield return new WaitForSeconds(0.5f);
+
+        nextButton.SetActive(true);
+        eventPos = 3;
+    }
+
+    IEnumerator EventThree()
+    {
+        nextButton.SetActive(false);
+        charFhósIdle.SetActive(false);
+        charFhósSpeak1.SetActive(true);
+        charThalassaSpeak1.SetActive(false);
+        charThalassaIdle.SetActive(true);
+
+        charName.GetComponent<TMPro.TMP_Text>().text = "Fhós";
+        textToSpeak = "As long as necessary.\r\nSomeone has to hold it together.";
+        textBox.GetComponent<TMPro.TMP_Text>().text = textToSpeak;
+        currentTextLenght = textToSpeak.Length;
+        TextCreator.runTextPrint = true;
+
+        yield return new WaitForSeconds(0.05f);
+        yield return new WaitForSeconds(1);
+        yield return new WaitUntil(() => textLenght == currentTextLenght);
+        yield return new WaitForSeconds(0.3f);
+
+        nextButton.SetActive(true);
+        eventPos = 4;
+    }
+
+
+    public void NextButton()
+    {
+        if (eventPos == 1) StartCoroutine(EventOne());
+        else if (eventPos == 2) StartCoroutine(EventTwo());
+        else if (eventPos == 3) StartCoroutine(EventThree());        
+        
     }
 }
